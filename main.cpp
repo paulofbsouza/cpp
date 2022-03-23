bc6e1bd90778b5afac2615cbc009e8296b752dab
Paulo Fernandes Barbosa de Souza Jr <55068173+paulofbsouza@users.noreply.github.com>
2022-03-23 17:10:03 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Paulo De Souza Jr on 2/17/22.
+//  Copyright © 2022 Paulo De Souza Jr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a;
+    std::cout << "Hello, World!\n";
+    std::cout << "This is a test, using the MAC OS Xcode\n";
+    std::cout << "Apparently: It works well, by Mr. de Souza\n";
+    std::cout << "Please, enter with a number:" ;
+    std::cin >> a;
+    std::cout << "\nYou type:" << a <<"\n";
+    return 0;
+}
