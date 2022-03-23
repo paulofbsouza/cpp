9c56456199f48246caa5c4254e5384e9ae8b31e1
Paulo Fernandes Barbosa de Souza Jr <55068173+paulofbsouza@users.noreply.github.com>
2022-03-23 17:08:39 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  While
+//
+//  Created by Paulo De Souza Jr on 2/22/22.
+//  Copyright © 2022 Paulo De Souza Jr. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int i,a;
+    i=1;
+
+    cout<< "Entrez avec un valeur:\n";
+    cin>> argc;
+    cout<<"\n";
+    while (i<=argc){
+        for (a=1;a<=3;a++){
+            cout<< i<<" "<<a<<"\n";
+
+        }
+        cout<< "\n";
+        i++;
+    }
+    return 0;
+}
